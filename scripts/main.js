e528bcb84f102077df7f7eef71ca26bad82ba484
MU94W <nanqiao15@126.com>
2017-03-25 17:13:17 +0800

    now can write selected score

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,30 @@
-// config object
+// config object example
 var config = {
     baseurl:'data',
     exps:[
         {
             name:'ABX',
+            path:'ABX',
             styles:['Emphasis','Neutral'],
             files:['Test_1.wav','Test_3.wav','Test_6.wav','Test_15.wav','Test_17.wav'],
         },
         {
             name:'MOS',
+            path:'MOS',
             styles:['Natural','Neutral','Emphasis'],
-            files:['Test_21.wav','Test_27.wav','Test_42.wav','Test_43.wav','Test_44.wav'],
+            files:['Test_21.wav','Test_27.wav','Test_42.wav','Test_43.wav','Test_44.wav','Test_57.wav','Test_66.wav','Test_77.wav','Test_83.wav','Test_90.wav'],
         },
     ],
 };
-// config object
+// config object example
+
+var audio_prefix = 'aud_';
 
 var linVector = function(start, stop, interval=1){
     var len = Math.floor((stop - start - 1)/interval) + 1;
     var ret = new Int32Array(len);
     for(var i = 0; i < len; i++){
-        ret[i] = start + i * interval;
+        ret[i] = start + i * interval
     }
     return ret;
 };
@@ -48,43 +52,121 @@ var getRandomColor = function(){
         })('');
 };
 
-var create_EVAL_HTML = function(dic,exp_order){
+var createEvalHtml = function(dic,exp_order){
     var style_cnt = dic.styles.length;
     var tmp = "";
     for(var i = 0; i < style_cnt; i++){
-        tmp += "<audio src='' controls class='EVAL' id='exp_" + exp_order + "_" + i +"'></audio>";
+        tmp += "<div class='audio_wrapper'>";
+        tmp += "<audio src='' controls class='EVAL' id='" + audio_prefix + exp_order + "_" + i + "'></audio>";
+        tmp += "<select class='selector' id='sel_" + exp_order + "_" + i + "'>"
+                    + "<option value='1'> bad </option>"
+                    + "<option value='2'> poor </option>"
+                    + "<option value='3' selected> fair </option>"
+                    + "<option value='4'> good </option>"
+                    + "<option value='5'> excellent </option>"
+                + "</select>";
+        tmp += "</div>";
     }
     var str_prev = "<div class='btn' id='exp_p" + exp_order + "'>Prev</div>";
     var str_next = "<div class='btn' id='exp_n" + exp_order + "'>Next</div>";
-    var str = "<div class='EVAL_wrapper'>" + tmp + str_prev + str_next + "</div>";
+    var str_step = "<div class='box_step' id='exp_s" + exp_order + "'>Audios:" + dic.files.length + "</div>";
+    var str_btns = "<div class='btn_wrapper'>" + str_prev + str_step + str_next + "</div>"
+    var str_okay = "<div class='btn' id='ok_" + exp_order + "'>Okay</div>";
+    var str = "<div class='EVAL_wrapper' id='exp_w" + exp_order + "'>" + str_btns + tmp + str_okay + "</div>";
     return str;
 };
 
-var add_eval_func = function(dom_obj,dic,exp_order){
-    var inner_html = create_EVAL_HTML(dic,exp_order);
+var createEvalSelHtml = function(dic,exp_order){
+    var show_exp_name = "Exp " + exp_order + ": " + dic.name;
+    var str = "<div class='btn' id='sel_" + exp_order + "'>" + show_exp_name + "</div>";
+    return str;
+};
+
+var addInnerHtml = function(func,dom_obj,dic,exp_order){
+    var inner_html = func(dic,exp_order);
     dom_obj.innerHTML += inner_html;
 };
 
+var eval_selector = document.getElementById("eval_selector");
+(function(){
+    for(var i = 0; i < config.exps.length; i++){
+        addInnerHtml(createEvalSelHtml,eval_selector,config.exps[i],i);
+    }
+})();
+
 var eval_container = document.getElementById("main_eval_container");
-for(var i = 0; i < config.exps.length; i++){
-    add_eval_func(eval_container,config.exps[i],i);
-};
+(function(){
+    for(var i = 0; i < config.exps.length; i++){
+        addInnerHtml(createEvalHtml,eval_container,config.exps[i],i);
+    }
+})();
 
 // global log all the exps' step
 var init_cnt = new Int32Array(config.exps.length);
+(function(){
+    for(var i = 0; i < config.exps.length; i++){
+        init_cnt[i] = -1;
+    }
+})();
+
 var audi_lst = new Array(config.exps.length);
+
 for(var i = 0; i < config.exps.length; i++){
     audi_lst[i] = config.exps[i].files;
 }
+
 var perm_lst = new Array(config.exps.length);
+
 (function(){
     for(var i = 0; i < config.exps.length; i++){
         perm_lst[i] = linVector(0,config.exps[i].files.length);
         permutation(perm_lst[i]);
-        //console.log(perm_lst[i]);
     }
 })();
 
+var getAudioPath = function(exp_order, style_order, step){
+    return config.baseurl + '/' + config.exps[exp_order].path
+            + '/' + config.exps[exp_order].styles[style_order]
+            + '/' + audi_lst[exp_order][perm_lst[exp_order][step]];
+};
+
+var setAudioPath = function(exp_order, style_order, step){
+    var this_audio_id = audio_prefix + exp_order + '_' + style_order;
+    var this_audio = document.getElementById(this_audio_id);
+    var audio_path = getAudioPath(exp_order, style_order, step);
+    this_audio.src = audio_path;
+};
+
+var global_file_step;   // cache where i'm 
+
+var updateStepShow = function(exp_order, step){
+    global_file_step = step;
+    var this_step_show_id = 'exp_s' + exp_order;
+    var this_step_show = document.getElementById(this_step_show_id);
+    this_step_show.innerHTML = (step+1) + ' / ' + config.exps[exp_order].files.length;
+};
+
+// Button 'Exp n: ##': add event(onclick) handle
+(function(){
+    for(var i = 0; i < config.exps.length; i++){
+        var sel_id = 'sel_' + i;
+        var sel_btn = document.getElementById(sel_id);
+        sel_btn.show_id = 'exp_w' + i;
+        sel_btn.onclick = function(){
+            // turn off all EVAL_wrapper divs' show
+            for(var i = 0; i < config.exps.length; i++){
+                var eval_w_id = 'exp_w' + i;
+                var eval_w = document.getElementById(eval_w_id);
+                eval_w.style.display = 'none';
+            }
+            // turn on the EVAL_wrapper div's show controlled by this btn
+            var eval_w = document.getElementById(this.show_id);
+            eval_w.style.display = 'flex';
+        };
+    }
+})();
+
+// Button 'Prev' && 'Next': add event(onclick) handle
 (function(){
     for(var i = 0; i < config.exps.length; i++){
         var btn_p_id = 'exp_p' + i;
@@ -98,46 +180,72 @@ var perm_lst = new Array(config.exps.length);
             var exp_order = this.exp_order;
             var step = init_cnt[exp_order];
             var audios = config.exps[exp_order].files.length;
-            for(var j = 0; j < config.exps[exp_order].styles.length; j++){
-                var this_audio_id = 'exp_' + exp_order + '_' + j;
-                var this_audio = document.getElementById(this_audio_id);
-                var audio_path = config.baseurl + '/' + config.exps[exp_order].name
-                                    + '/' + config.exps[exp_order].styles[j]
-                                    + '/' + audi_lst[exp_order][perm_lst[exp_order][step]];
-                this_audio.setAttribute('src',audio_path);
+            if(step > 0){
+                step -= 1;
+                for(var j = 0; j < config.exps[exp_order].styles.length; j++){
+                    setAudioPath(exp_order, j, step);
+                    updateStepShow(exp_order, step);
+                }
+                init_cnt[exp_order] = step;
             }
-            step = (step - 1) % audios;
-            if(step < 0){
-                step += audios;
-            }
-            init_cnt[exp_order] = step;
         };
         btn_n.onclick = function(){
             var exp_order = this.exp_order;
             var step = init_cnt[exp_order];
             var audios = config.exps[exp_order].files.length;
-            for(var j = 0; j < config.exps[exp_order].styles.length; j++){
-                var this_audio_id = 'exp_' + exp_order + '_' + j;
-                var this_audio = document.getElementById(this_audio_id);
-                var audio_path = config.baseurl + '/' + config.exps[exp_order].name
-                                    + '/' + config.exps[exp_order].styles[j]
-                                    + '/' + audi_lst[exp_order][perm_lst[exp_order][step]];
-                this_audio.setAttribute('src',audio_path);
+            if(step < audios-1){
+                step += 1;
+                for(var j = 0; j < config.exps[exp_order].styles.length; j++){
+                    setAudioPath(exp_order, j, step);
+                    updateStepShow(exp_order, step);
+                }
+                init_cnt[exp_order] = step;
             }
-            step = (step + 1) % audios;
-            init_cnt[exp_order] = step;
         };
- 
     }
 })();
 
-var myHeading = document.querySelector("h1");
-var myHeadingStaticStyle = myHeading.getAttribute('style');
-var cnt = 0;
+// value_arr's dims: 
+//                  1st-dim: exp_order
+//                  2nd-dim: file_order [dict]
+//                  3rd-dim: style_order [dict]
+var value_dic = {};
+(function(){
+    for(var i = 0; i < config.exps.length; i++){
+        value_dic[config.exps[i].path] = {};
+        for(var fl = 0; fl < config.exps[i].files.length; fl++){
+            value_dic[config.exps[i].path][config.exps[i].files[fl]] = {};
+        }
+    }
+})();
+
+// Button 'Okay': add event(onclick) handle
+(function(){
+    for(var i = 0; i < config.exps.length; i++){
+        var btn_ok_id = 'ok_' + i;
+        var btn_ok = document.getElementById(btn_ok_id);
+        btn_ok.exp_id = i;
+        btn_ok.onclick = function(){
+            var styles = config.exps[this.exp_id].styles.length;
+            for(var i = 0; i < styles; i++){
+                var sel_id = 'sel_' + this.exp_id + '_' + i;
+                var sel_val = document.getElementById(sel_id).value;
+                // put sel_val to value_arr
+                // get audio path and then split it
+                var aud_id = audio_prefix + this.exp_id + '_' + i;
+                var aud_obj = document.getElementById(aud_id);
+                var aud_path = aud_obj.src.split('/');
+                var aud_path_len = aud_path.length;
+                var file_name = aud_path[aud_path_len - 1];
+                var style_name = aud_path[aud_path_len - 2];
+                var exp_name = aud_path[aud_path_len - 3];
+                value_dic[exp_name][file_name][style_name] = parseInt(sel_val);
+            }
+        };
+    }
+})();
+
+var myHeading = document.querySelector("p");
 myHeading.onclick = function(){
-    var res = cnt + Math.round(Math.random()*10);
-    this.textContent = res;
-    var color = 'color:' + getRandomColor()
-    this.setAttribute('style', myHeadingStaticStyle + ';' + color);
-    cnt++;
+    this.style.color = getRandomColor();
 };
